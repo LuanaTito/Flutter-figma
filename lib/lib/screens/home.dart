70cf3e43cfac054c9014a93cc0a4b30ac6dc8f5e
LuanaTito <111540897+LuanaTito@users.noreply.github.com>
2023-04-18 11:24:21 -0300

    atualizacao lib

---
+++ lib/lib/screens/home.dart
@@ -0,0 +1,20 @@
+import 'package:alubank/components/sections/header.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+const Home({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(body: Column(
+      children: <widget>[
+        Header(),
+        BoxCard(boxContent:  Text('Olá Mundo!')) //para ver se está funcionando
+      ]
+     ), ); 
+     }
+}
+//envolver o head em uma coluna assim ele será alinhado (mais grudado)
+//o rows irá expandir 
+//widgets de organização de posicinamento
+//executar e ver a diferença
